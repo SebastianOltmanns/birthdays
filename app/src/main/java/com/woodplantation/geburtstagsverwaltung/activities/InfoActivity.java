6776d9446bca8a5f058ba6fecb6ce4f9849376fd
Sebastian Oltmanns <sebastian.oltmanns.developer@gmail.com>
2018-08-29 11:45:48 +0200

    changed app version, removed unnecessary imports in one file

--- app/src/main/java/com/woodplantation/geburtstagsverwaltung/activities/InfoActivity.java
+++ app/src/main/java/com/woodplantation/geburtstagsverwaltung/activities/InfoActivity.java
@@ -1,23 +1,16 @@
 package com.woodplantation.geburtstagsverwaltung.activities;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.Html;
-import android.text.Spannable;
-import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.webkit.WebView;
 import android.text.util.Linkify;
 import android.view.View;
-import android.widget.ScrollView;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import com.woodplantation.geburtstagsverwaltung.BuildConfig;
@@ -25,14 +18,10 @@ import com.woodplantation.geburtstagsverwaltung.R;
 
 import org.xml.sax.XMLReader;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 
 /**
  * Created by Sebu on 22.03.2016.
@@ -159,7 +148,7 @@ public class InfoActivity extends AppCompatActivity {
 		}
 	}
 
-	protected void openDialog(View view) {
+	public void openDialog(View view) {
 		switch(view.getId()) {
 			case R.id.information_activity_about_button: {
 				aboutDialog.show();
