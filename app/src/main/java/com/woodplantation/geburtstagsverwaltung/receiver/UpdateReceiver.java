9ef5f1ddf05df19df6da3dffd6e615e37ced391e
Sebastian Oltmanns <sebastian.oltmanns.developer@gmail.com>
2020-12-27 10:37:08 +0100

    migration work is done in update receiver

---
+++ app/src/main/java/com/woodplantation/geburtstagsverwaltung/receiver/UpdateReceiver.java
@@ -0,0 +1,127 @@
+package com.woodplantation.geburtstagsverwaltung.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.util.Log;
+
+import com.woodplantation.geburtstagsverwaltung.R;
+import com.woodplantation.geburtstagsverwaltung.model.Entry;
+import com.woodplantation.geburtstagsverwaltung.notifications.AlarmCreator;
+import com.woodplantation.geburtstagsverwaltung.repository.Repository;
+import com.woodplantation.geburtstagsverwaltung.storage.DataSet;
+import com.woodplantation.geburtstagsverwaltung.storage.StorageHandler;
+import com.woodplantation.geburtstagsverwaltung.util.MyPreferences;
+import com.woodplantation.geburtstagsverwaltung.widget.WidgetAlarmReceiver;
+import com.woodplantation.geburtstagsverwaltung.widget.WidgetService;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.stream.Collectors;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+/**
+ * Created by Sebu on 23.08.2017.
+ * Contact: sebastian.oltmanns.developer@googlemail.com
+ */
+@AndroidEntryPoint
+public class UpdateReceiver extends BroadcastReceiver {
+
+    @Inject
+    MyPreferences preferences;
+    @Inject
+    StorageHandler storageHandler;
+    @Inject
+    MyPreferences myPreferences;
+    @Inject
+    Context context;
+    @Inject
+    Repository repository;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        AlarmCreator.createFromScratch(context, preferences);
+        WidgetAlarmReceiver.createNextAlarm(context);
+        WidgetService.notifyDataChanged(context);
+
+        migrateTwoPreferencesToOne();
+        migrateStorageToDb();
+    }
+
+
+    private void migrateStorageToDb() {
+        if (!myPreferences.isStorageMigrated()) {
+            // load current data
+            ArrayList<DataSet> data = storageHandler.loadData();
+
+            // iterate list and insert into db
+            repository.insertData(
+                    data.stream()
+                            .map(dataSet -> {
+                                Entry entry = new Entry();
+                                entry.firstName = dataSet.firstName;
+                                entry.lastName = dataSet.lastName;
+                                entry.ignoreYear = false;
+                                entry.birthday = LocalDate.of(dataSet.birthday.get(Calendar.YEAR), dataSet.birthday.get(Calendar.MONTH), dataSet.birthday.get(Calendar.DAY_OF_MONTH));
+                                entry.notes = dataSet.others;
+                                return entry;
+                            })
+                            .collect(Collectors.toSet()),
+                    () -> {
+                        // store in preferences that we did migration
+                        myPreferences.preferences
+                                .edit()
+                                .putBoolean(context.getString(R.string.preferences_storage_migrated), true)
+                                .apply();
+
+                        // remove previous storage file
+                        try {
+                            context.deleteFile(StorageHandler.filePath);
+                        } catch (Exception ignored) {
+                        }
+                    }
+            );
+        }
+    }
+
+    private void migrateTwoPreferencesToOne() {
+        if (!myPreferences.arePreferencesMigrated()) {
+            // get notifications preferences
+            @SuppressWarnings("deprecation")
+            MyPreferences notificationPreferences = MyPreferences.getNotificationPreferences(context);
+
+            myPreferences.preferences
+                    .edit()
+                    // migrate all notification preferences to default preferences
+                    .putBoolean(context.getString(R.string.preferences_active), notificationPreferences.getActive())
+                    .putBoolean(context.getString(R.string.preferences_on_birthday_active), notificationPreferences.getOnBirthdayActive())
+                    .putBoolean(context.getString(R.string.preferences_one_day_before_active), notificationPreferences.getOneDayBeforeActive())
+                    .putBoolean(context.getString(R.string.preferences_x_days_before_active), notificationPreferences.getXDaysBeforeActive())
+                    .putInt(context.getString(R.string.preferences_on_birthday_clock), notificationPreferences.getOnBirthdayClock())
+                    .putInt(context.getString(R.string.preferences_one_day_before_clock), notificationPreferences.getOneDayBeforeClock())
+                    .putInt(context.getString(R.string.preferences_x_days_before_clock), notificationPreferences.getXDaysBeforeClock())
+                    .putInt(context.getString(R.string.preferences_x_days_before_days), notificationPreferences.getXDaysBeforeDays())
+                    .putInt(
+                            context.getString(R.string.preferences_notification_id),
+                            notificationPreferences.preferences.getInt(
+                                    context.getString(R.string.preferences_notification_id),
+                                    context.getResources().getInteger(R.integer.preferences_notification_id)
+                            )
+                    )
+                    // store that we did migration
+                    .putBoolean(context.getString(R.string.preferences_preferences_migrated), true)
+                    .apply();
+
+            // delete notification preferences if sdk is new enough
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                MyPreferences.removeNotificationsPreferences(context);
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/woodplantation/geburtstagsverwaltung/repository/Repository.java
+++ app/src/main/java/com/woodplantation/geburtstagsverwaltung/repository/Repository.java
@@ -1,120 +1,37 @@
 package com.woodplantation.geburtstagsverwaltung.repository;
 
-import android.content.Context;
-import android.os.Build;
-
 import androidx.lifecycle.LiveData;
 
-import com.woodplantation.geburtstagsverwaltung.R;
 import com.woodplantation.geburtstagsverwaltung.database.EntryDao;
 import com.woodplantation.geburtstagsverwaltung.model.Entry;
-import com.woodplantation.geburtstagsverwaltung.storage.DataSet;
-import com.woodplantation.geburtstagsverwaltung.storage.StorageHandler;
-import com.woodplantation.geburtstagsverwaltung.util.MyPreferences;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.inject.Inject;
 
-import dagger.hilt.android.qualifiers.ApplicationContext;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Action;
 
 public class Repository {
 
     private final EntryDao entryDao;
-    private final StorageHandler storageHandler;
-    private final MyPreferences myPreferences;
-    private final Context context;
     //TODO cancel compositedisposable when application closes
-    private final CompositeDisposable compositeDisposable = new CompositeDisposable();
+    public final CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Inject
-    public Repository(EntryDao entryDao, StorageHandler storageHandler, MyPreferences myPreferences, @ApplicationContext Context context) {
+    public Repository(EntryDao entryDao) {
         this.entryDao = entryDao;
-        this.storageHandler = storageHandler;
-        this.myPreferences = myPreferences;
-        this.context = context;
-
-        migrateTwoPreferencesToOne();
-        migrateStorageToDb();
-    }
-
-    private void migrateStorageToDb() {
-        if (!myPreferences.isStorageMigrated()) {
-            // load current data
-            ArrayList<DataSet> data = storageHandler.loadData();
-
-            // iterate list and insert into db
-            compositeDisposable.add(entryDao.insertMany(data
-                    .stream()
-                    .map(dataSet -> {
-                        Entry entry = new Entry();
-                        entry.firstName = dataSet.firstName;
-                        entry.lastName = dataSet.lastName;
-                        entry.ignoreYear = false;
-                        entry.birthday = LocalDate.of(dataSet.birthday.get(Calendar.YEAR), dataSet.birthday.get(Calendar.MONTH), dataSet.birthday.get(Calendar.DAY_OF_MONTH));
-                        entry.notes = dataSet.others;
-                        return entry;
-                    })
-                    .collect(Collectors.toSet())
-            ).subscribe(() -> {
-                // store in preferences that we did migration
-                myPreferences.preferences
-                        .edit()
-                        .putBoolean(context.getString(R.string.preferences_storage_migrated), true)
-                        .apply();
-
-                // remove previous storage file
-                try {
-                    context.deleteFile(StorageHandler.filePath);
-                } catch (Exception ignored) {
-                }
-            }));
-        }
-    }
-
-    private void migrateTwoPreferencesToOne() {
-        if (!myPreferences.arePreferencesMigrated()) {
-            // get notifications preferences
-            @SuppressWarnings("deprecation")
-            MyPreferences notificationPreferences = MyPreferences.getNotificationPreferences(context);
-
-            myPreferences.preferences
-                    .edit()
-                    // migrate all notification preferences to default preferences
-                    .putBoolean(context.getString(R.string.preferences_active), notificationPreferences.getActive())
-                    .putBoolean(context.getString(R.string.preferences_on_birthday_active), notificationPreferences.getOnBirthdayActive())
-                    .putBoolean(context.getString(R.string.preferences_one_day_before_active), notificationPreferences.getOneDayBeforeActive())
-                    .putBoolean(context.getString(R.string.preferences_x_days_before_active), notificationPreferences.getXDaysBeforeActive())
-                    .putInt(context.getString(R.string.preferences_on_birthday_clock), notificationPreferences.getOnBirthdayClock())
-                    .putInt(context.getString(R.string.preferences_one_day_before_clock), notificationPreferences.getOneDayBeforeClock())
-                    .putInt(context.getString(R.string.preferences_x_days_before_clock), notificationPreferences.getXDaysBeforeClock())
-                    .putInt(context.getString(R.string.preferences_x_days_before_days), notificationPreferences.getXDaysBeforeDays())
-                    .putInt(
-                            context.getString(R.string.preferences_notification_id),
-                            notificationPreferences.preferences.getInt(
-                                    context.getString(R.string.preferences_notification_id),
-                                    context.getResources().getInteger(R.integer.preferences_notification_id)
-                            )
-                    )
-                    // store that we did migration
-                    .putBoolean(context.getString(R.string.preferences_preferences_migrated), true)
-                    .apply();
-
-            // delete notification preferences if sdk is new enough
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                MyPreferences.removeNotificationsPreferences(context);
-            }
-        }
     }
 
     public LiveData<List<Entry>> getData() {
         return entryDao.getAll();
+    }
+
+    public void insertData(Set<Entry> data, Action onComplete) {
+        compositeDisposable.add(
+                entryDao.insertMany(data).subscribe(onComplete)
+        );
     }
 
 }
